884a8efb614e984d38ece87d9eb6817a69ff7760
jchck <hey@justinchick.com>
2015-08-10 07:00:55 -0500

    write gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp 		= require('gulp'),
+	rename 		= require('gulp-rename'),
+	minify		= require('gulp-minify-css'),
+	sass		= require('gulp-sass'),
+	size		= require('gulp-size');
+
+gulp.task('css', function(){
+	gulp.src('./src/spacing.scss')
+		.pipe(sass())
+		.pipe(size({gzip: true, showFiles: true, title:'prefixed'}))
+		.pipe(gulp.dest('./dest'))
+		.pipe(minify())
+		.pipe(size({gzip: true, showFiles: true, title:'minified'}))
+		.pipe(rename({ extname: '.min.css' }))
+		.pipe(gulp.dest('./dest'));
+});
+
+gulp.task('default', ['css']);
